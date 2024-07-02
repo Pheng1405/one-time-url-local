6a0fb7f26ce8b7694605799cb6e283d57d707d04
Seng Sopheng <phengg1405@gmail.com>
2024-07-02 14:21:54 +0700

    kick start one time url in local express

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+const port = 3000;
+
+// will store this in db later
+const tokenStore: { [key: string]: boolean } = {};
+
+// Middleware to check if the token has been used
+function checkToken(req: Request, res: Response, next: NextFunction) {
+    console.log(tokenStore);
+    const token = req.params.token;
+    if (tokenStore[token]) {
+        return res.status(403).send('This link has already been used.');
+    }
+    next();
+}
+
+// Generate URL route
+// Will check if url exist later
+app.get('/generate', (req: Request, res: Response) => {
+    // use uuid later
+    const token = Math.random().toString(36).substr(2);
+    tokenStore[token] = false;
+    res.send(`Your one-time URL is: http://localhost:${port}/link/${token}`);
+});
+
+// Route to use the one-time URL
+app.get('/link/:token', checkToken, (req: Request, res: Response) => {
+    const token = req.params.token;
+    if (tokenStore[token] === false) {
+        tokenStore[token] = true;  // Mark token as used
+        res.send('You are first person who open this link');
+    } else {
+        res.status(403).send('This link has already been used.');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
