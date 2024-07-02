f890b6cfd4450d6cbfbf570b913345c9854d49b5
Seng Sopheng <phengg1405@gmail.com>
2024-07-02 14:29:43 +0700

    update command

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,9 @@ import express, { Request, Response, NextFunction } from 'express';
 const app = express();
 const port = 3000;
 
-// will store this in db later
+// store in db later
 const tokenStore: { [key: string]: boolean } = {};
 
-// Middleware to check if the token has been used
 function checkToken(req: Request, res: Response, next: NextFunction) {
     console.log(tokenStore);
     const token = req.params.token;
@@ -16,20 +15,20 @@ function checkToken(req: Request, res: Response, next: NextFunction) {
     next();
 }
 
-// Generate URL route
-// Will check if url exist later
+// url gen
+// use uuid later
 app.get('/generate', (req: Request, res: Response) => {
-    // use uuid later
+    
     const token = Math.random().toString(36).substr(2);
     tokenStore[token] = false;
     res.send(`Your one-time URL is: http://localhost:${port}/link/${token}`);
 });
 
-// Route to use the one-time URL
+//open link
 app.get('/link/:token', checkToken, (req: Request, res: Response) => {
     const token = req.params.token;
     if (tokenStore[token] === false) {
-        tokenStore[token] = true;  // Mark token as used
+        tokenStore[token] = true;
         res.send('You are first person who open this link');
     } else {
         res.status(403).send('This link has already been used.');
